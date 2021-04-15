c23539836820749014a93a0dcbb2bfdfa4ce39e9
Lionel Humbert <helloliohumb@gmail.com>
2021-04-15 18:52:02 +0200

    00_journalists • master finish

--- lib/00_journalists.rb
+++ lib/00_journalists.rb
@@ -1,59 +1,132 @@
+def handle
 arr = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+  return arr
+end
 
-#Combien de handle dans l'array?
-puts "Combien y a t il de handle dans ce array ?"
-puts arr.length
-
-#Quel est le handle le plus court?
-puts "Quel est le handle le plus court dans cette liste ?"
-puts arr.min { |a, b| a.length <=> b.length}
-
-#Combien de handle contenant 5 caractères?
-puts "Combien y a t il de handle contenant 5 caractères ?"
-num = 0
-arr.each do |i|
-    if (i.length - 1) == 5
-      num = num + 1
+def questions
+  puts "Hello, choisi le numéro qui correspond à la question de ton choix :"
+  puts "• 1 • Combien y a t-il de handle dans cette array ?"
+  puts "• 2 • Quel est le handle le plus court de cette liste ?"
+  puts "• 3 • Combien y a t-il de handle contenant 5 caractères ?"
+  puts "• 4 • Combien commencent par une majuscule ?"
+  puts "• 5 • Pour trier la liste de handle par ordre alphabétique ."
+  puts "• 6 • Pour trier les handle par taille (du plus petit nombre de caractère au plus grand) ."
+  puts "• 7 • Quelle est la position dans l'array de la personne se nommant @epenser ?"
+  puts "• 8 • Pour avoir le nombre de handle avec un certain nombre de caractère ."
+  puts "• 0 • Pour sortir ."
+  print "> "
+  question = gets.chomp.to_i
+  if question >= 0 && question <= 8
+    return question
+  else
+    puts "• Ceci n'est pas un bon numéro, reesaye ! •"
+    return (-1)
   end
 end
-puts num
+
+#Combien de handle dans l'array?
+def nombre(arr)
+  puts "• Combien y a t-il de handle dans cette array ? •"
+  puts arr.length
+end
+
+#Quel est le handle le plus court?
+def court(arr)
+  puts "• Quel est le handle le plus court de cette liste ? •"
+  puts arr.min { |a, b| a.length <=> b.length}
+end
+
+#Combien de handle contenant 5 caractères?
+def caractere(arr)
+  puts "• Combien y a t-il de handle contenant 5 caractères ? •"
+  num = 0
+  arr.each do |i|
+      if (i.length - 1) == 5
+        num = num + 1
+    end
+  end
+  puts num
+end
 
 #Combien commencent par une majuscule?
-puts "Combien de handle commencent par une majuscule ?"
-letter_maj = arr.select {|word| word.start_with?(/@[[:upper:]]/)}
-puts letter_maj.length
+def majuscule(arr)
+  puts "• Combien commencent par une majuscule ? •"
+  letter_maj = arr.select {|word| word.start_with?(/@[[:upper:]]/)}
+  puts letter_maj.length
+end
 
 #Trie la liste de handle par ordre alphabétique.
-puts "La liste de handle par ordre alphabétique."
-alphab = arr.sort_by {|word| word.downcase}
-alphab.each do |i|
-  puts i
+def alphabetique(arr)
+  puts "• Voici la liste des handles par ordre alphabétique : •"
+  alphab = arr.sort_by {|word| word.downcase}
+  alphab.each do |i|
+    puts i
+  end
 end
 
 #Trie la liste de handle par taille des handles.
-puts "La liste de handle par taille"
-list = arr.sort_by {|letter| letter.length}
-list.each do |i|
-  puts i
+def taille(arr)
+  puts "• Voici la liste des handles par taille : •"
+  list = arr.sort_by {|letter| letter.length}
+  list.each do |i|
+    puts i
+  end
 end
 
 #Quelle est la position dans l'array de la personne @epenser.
-puts "La position de la personne @epenser"
-puts arr.index("@epenser")
+def epenser(arr)
+  puts "• J'ai retrouvé @epenser, il se trouve en position : •"
+  puts arr.index("@epenser")
+end
 
 #Sors moi une répartition des handle par taille de ces derniers.
-puts "La répartition par taille des handles"
-x = 0
-length = arr.length
-y = 2
-while x != length
-    z = 0
-    arr.each do |i|
-        if (i.length) == y
-          z = z + 1
-        end
-    end
-    puts "Il y a #{z} handle(s) de #{y - 1} caractère(s)"
-    y = y + 1
-    x = x + z
+def repartitions(arr)
+  puts "• Voici le nombre de handle avec un certain nombre de caractère : •"
+  x = 0
+  length = arr.length
+  y = 2
+  while x != length
+      z = 0
+      arr.each do |i|
+          if (i.length) == y
+            z = z + 1
+          end
+      end
+      puts "Il y a #{z} handle(s) de #{y - 1} caractère(s)"
+      y = y + 1
+      x = x + z
+  end
 end
+
+#Les réponses aux questions
+def reponse(question, arr)
+  if question == 1
+    nombre(arr)
+  elsif question == 2
+    court(arr)
+  elsif question == 3
+    caractere(arr)
+  elsif question == 4
+    majuscule(arr)
+  elsif question == 5
+    alphabetique(arr)
+  elsif question == 6
+    taille(arr)
+  elsif question == 7
+    epenser(arr)
+  elsif question == 8
+    repartitions(arr)
+  end
+  puts ""
+end
+
+def go
+  arr = handle
+  while ((question = questions) !=0)
+    reponse(question, arr)
+  end
+  puts ""
+  puts "• À bientôt . MERCI ! •"
+end
+
+go
